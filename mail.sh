cabd691b3aa34801de2e17f740422366609a6627
Dan Guest <dguest@cern.ch>
2015-10-09 12:16:18 +0200

    cc lar

--- mail.sh
+++ mail.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SUB=$(echo "it's " `date` ", get your shit together")
-mail -s "$SUB" -c dhguest@gmail.com jane.cummings@yale.edu < <(echo "this is what jane has done"; ls -ltr  ~cummings/)
+mail -s "$SUB" -c dhguest@gmail.com -c lawrence.lee.jr@cern.ch jane.cummings@yale.edu < <(echo "this is what jane has done"; ls -ltr  ~cummings/)
